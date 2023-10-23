858c5af5860e3cf6313a824928353a24bb1dce38
EDB999 <113853336+EDB999@users.noreply.github.com>
2023-10-23 23:02:56 +0300

    init: start Task

--- Task1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.task1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.task1 to javafx.fxml;
-    exports com.example.task1;
-}
--- Task2/src/main/java/Factory/IFactory.java
+++
@@ -1,8 +0,0 @@
-package Factory;
-
-import model.Shape;
-
-public interface IFactory {
-    Shape CreateShape(String figure);
-}
-
